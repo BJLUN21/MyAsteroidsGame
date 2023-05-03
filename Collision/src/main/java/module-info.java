e56278f34d48135a1017d410fae6ce7347111b4e
BJLUN21 <bjlun21@student.sdu.dk>
2023-05-03 21:49:03 +0200

    Revert "Added MyAsteroidsGame project"
    This reverts commit 4611642b6b15c8d4a6fb3b89d0b6df9fabda7ea6.

--- Collision/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-
-module Collision {
-    requires Common;
-    requires CommonAsteroid;
-
-    provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.collisionsystem.CollisionDetector;
-}
